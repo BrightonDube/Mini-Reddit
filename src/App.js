ff666bf1e79d2ad8d6209c2871f368f13b6ac8ea
BrightonDube <BrightonDube@users.noreply.github.com>
2020-05-31 14:45:00 +0200

    change card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//jshint esversion: 9
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import isImageUrl from "is-image-url";
+import Post from "./Card";
 
-function App() {
+function Reddit() {
+  //Fetch any subreddit
+  const [typedName, setTypedName] = useState("");
+  const [subreddit, setSubreddit] = useState("react");
+
+  const getSubreddit = () => {
+    setSubreddit(typedName);
+    setTypedName("");
+  };
+  //Fetch posts from subreddit and store them in state
+  const [posts, setPosts] = useState([]);
+
+  //useEffect is used to store the returned posts in state
+  //the second array argument is there to prevent infinite requests.
+  useEffect(() => {
+    //use fetch to get data from API and destructure it
+    getPosts();
+    async function getPosts() {
+      try {
+        const response = await fetch(
+          `https://www.reddit.com/r/${subreddit}.json`
+        );
+        const { data } = await response.json();
+        const { children } = data;
+        const posts = children.map((child) => child.data);
+        //The returned array has the data that we need to display.
+        setPosts(posts);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, [subreddit]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar mb-2 navbar-expand-lg navbar-light bg-light">
+        <h1 className="container">{subreddit}</h1>
+      </nav>
+      <input
+        type="text"
+        onChange={(event) => setTypedName(event.target.value)}
+        placeholder="Enter Subreddit"
+        className="form-control w-50"
+        value={typedName}
+      />
+      <button onClick={getSubreddit} className="btn m-3 btn-secondary">
+        Get Subreddit
+      </button>
+
+      <div className="container">
+        <h1 className="m-5 text-md-right">/r/{subreddit}</h1>
+        <div className="row">
+          {posts.map((post, index) => (
+            <div className="col col-12 col-md-6 col-lg-4 mb-4">
+              <Post
+                title={post.title}
+                //check if url links to an image or not
+                image={isImageUrl(`${post.url}`) ? post.url : null}
+                text={post.selftext}
+                author={post.author}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
+export default function App() {
+  return (
+    <div className="App">
+      <Reddit />
+    </div>
+  );
+}
---
+++ src/Card.jsx
@@ -0,0 +1,106 @@
+//jshint esversion: 9
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+const d = new Date();
+const date = d.getDate();
+const month = d.getMonth() + 1; // Since getMonth() returns month from 0-11 not 1-12
+const year = d.getFullYear();
+
+const dateStr = date + "/" + month + "/" + year;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+export default function Post({ title, image, text, author }) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={title}
+        subheader={dateStr}
+      />
+      <CardMedia className={classes.media} image={image} title={title} />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {text.substr(0, 100) + "..."}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>By: {author}</Typography>
+
+          <Typography paragraph>{text}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
